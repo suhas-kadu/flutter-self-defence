8d576edeaad95fd70ce491690b0e2cf74c8b29ef
suhas-kadu <kadusuhas72@gmail.com>
2021-03-04 01:58:21 +0530

    tip model created

---
+++ lib/data/tips.dart
@@ -0,0 +1,41 @@
+import '../model/tip_model.dart';
+
+List<Tip> getTips() {
+  List<Tip> _tips = new List();
+  Tip tip = new Tip();
+
+  tip.imgUrl = "assets/images/tip.jpg";
+  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+  _tips.add(tip);
+  tip = new Tip();  
+
+  tip.imgUrl = "assets/images/tip.jpg";
+  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+  _tips.add(tip);
+  tip = new Tip(); 
+
+  tip.imgUrl = "assets/images/tip.jpg";
+  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+  _tips.add(tip);
+  tip = new Tip(); 
+
+  tip.imgUrl = "assets/images/tip.jpg";
+  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+  _tips.add(tip);
+  tip = new Tip(); 
+
+  tip.imgUrl = "assets/images/tip.jpg";
+  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+  _tips.add(tip);
+  tip = new Tip(); 
+
+  tip.imgUrl = "assets/images/tip.jpg";
+  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+  _tips.add(tip);
+  tip = new Tip(); 
+
+  return _tips;
+
+
+
+}
---
+++ lib/model/tip_model.dart
@@ -0,0 +1,7 @@
+class Tip {
+  String imgUrl;
+  String description;
+
+  Tip({ this.imgUrl ,this.description});
+}
+
