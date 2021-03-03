58618e4bb6270b3ef83a3a3f5217db5188c33268
suhas-kadu <kadusuhas72@gmail.com>
2021-03-04 01:32:26 +0530

    homepage created

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -4,7 +4,61 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/images/img1.jpg",
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+            ),
+            Align(              
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RaisedButton(
+                  elevation: 5.0,
+                  color: Colors.white,
+                  textColor: Colors.black,                  
+                  onPressed: (){},
+                  child: Text(
+                    "Self Defence Tips",
+                  ),
+                  ),
+                  RaisedButton(
+                  elevation: 5.0,
+                  color: Colors.white,
+                  textColor: Colors.black,                  
+                  onPressed: (){},
+                  child: Text(
+                    "Self Defence Techniques",
+                  ),
+                  ),
+                  RaisedButton(
+                  elevation: 5.0,
+                  color: Colors.white,
+                  textColor: Colors.black,                  
+                  onPressed: (){},
+                  child: Text(
+                    "More...",
+                  ),
+                  ),
+                  RaisedButton(
+                  elevation: 5.0,
+                  color: Colors.white,
+                  textColor: Colors.black,                  
+                  onPressed: (){},
+                  child: Text(
+                    "HelpLine",
+                  ),
+                  ),
+                  
+
+                ],
+              ),
+            )
+        ],
+        ),
     );
   }
 }
