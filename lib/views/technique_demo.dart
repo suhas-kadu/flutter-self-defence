b2a9c8496e524db7a011c1a110ab5d1dff1c415a
suhas-kadu <kadusuhas72@gmail.com>
2021-03-05 02:40:30 +0530

    Technique demo screen added

---
+++ lib/views/technique_demo.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:self_defence/constants.dart';
+import 'package:self_defence/model/technique_model.dart';
+import 'package:video_player/video_player.dart';
+
+class TechniqueDemo extends StatefulWidget {
+  static const String id = "TechniqueDemo";
+
+  final Technique technique;
+
+  const TechniqueDemo({Key key, this.technique}) : super(key: key);
+
+  @override
+  _TechniqueDemoState createState() => _TechniqueDemoState();
+}
+
+class _TechniqueDemoState extends State<TechniqueDemo> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.network(
+      widget.technique.videoUrl,
+    );
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    _controller.setLooping(true);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        title: Text(widget.technique.title.toString()),
+      ),
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Padding(
+      padding:
+          const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          ),
+          SizedBox(height: 20),
+          Text(
+            widget.technique.description,
+            textAlign: TextAlign.justify,
+            style:kTextStyle,
+          ),
+        ],
+      ),
+    );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kPrimaryColor,
+        onPressed: () {
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
