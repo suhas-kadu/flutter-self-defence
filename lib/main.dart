000287c0eb8c47d6dbb6a37b82b15d43ebf52e6f
suhas-kadu <kadusuhas72@gmail.com>
2021-03-05 02:37:26 +0530

    new routes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:self_defence/views/helpline.dart';
 import 'package:self_defence/views/self_defence_techniques.dart';
 import 'package:self_defence/views/self_defence_tips.dart';
+import 'package:self_defence/views/technique_demo.dart';
 import 'views/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -16,7 +18,9 @@ class MyApp extends StatelessWidget {
         
         HomePage.id: (context) => HomePage(),
         SelfDefenceTips.id: (context) => SelfDefenceTips(),
-        SelfDefenceTechniques.id: (context) => SelfDefenceTechniques()
+        SelfDefenceTechniques.id: (context) => SelfDefenceTechniques(),
+        TechniqueDemo.id: (context) => TechniqueDemo(),
+        HelpLine.id: (context) => HelpLine(),
       },
       // home: HomePage(),
     );
