79489edbfcc039bea5766d2bb9718c5769e322a2
suhas-kadu <kadusuhas72@gmail.com>
2021-03-04 17:17:39 +0530

    techniques created

---
+++ lib/data/techniques.dart
@@ -0,0 +1,66 @@
+import '../model/technique_model.dart';
+
+List<Technique> getTechniques() {
+
+  List<Technique> _techniques = new List();
+  Technique technique = new Technique();
+
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+
+  technique = new Technique();
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+
+  technique = new Technique();
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+  
+    technique = new Technique();
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+
+    technique = new Technique();
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+
+    technique = new Technique();
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+
+    technique = new Technique();
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+
+    technique = new Technique();
+  technique.title = "Hammer Strike";
+  technique.imgUrl = "assets/images/tip.jpg";
+  technique.videoUrl = "https://thumbs.gfycat.com/FluffyFlashyCockroach-mobile.mp4";
+  technique.description = "Using your car keys is one of the easiest ways to defend yourself. Don’t use your fingernails, because you’re more at risk to injure your hands.\nInstead, if you feel unsafe while walking at night, have your keys stick out from one side of your fist for hammer strikes.\n\nTo perform:\n1.Hold your key ring in a tight fist, like holding a hammer, with keys extending from the side of your hand.\n2.Thrust downward toward your target.";
+  _techniques.add(technique);
+
+  return _techniques;
+
+
+}
