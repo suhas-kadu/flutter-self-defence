4d35e6d0c457361afa97439c80e409bdc9e7fbb2
suhas-kadu <kadusuhas72@gmail.com>
2021-03-05 02:41:32 +0530

    constants added

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6200EE);
+
+const kTextColor = Color(0xFF000000);
+
+const kTextStyle = TextStyle(
+  color: kTextColor,
+  fontSize: 16,
+);
+
+const kBoldText = TextStyle(fontWeight: FontWeight.bold);
