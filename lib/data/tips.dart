78d7245823a57e845054d7ca13ab0895b92d8c12
suhas-kadu <kadusuhas72@gmail.com>
2021-03-06 02:37:29 +0530

    tips list data updated

--- lib/data/tips.dart
+++ lib/data/tips.dart
@@ -4,42 +4,54 @@ List<Tip> getTips() {
   List<Tip> _tips = new List();
   Tip tip = new Tip();
 
-  tip.title = "Title";
-  tip.imgUrl = "assets/images/tip.jpg";
-  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+  tip.title = "Push back & yell loudly";
+  tip.imgUrl = "assets/images/tip1.jpg";
+  tip.description = "This the first thing you need to do as soon you realize that you are being stalked. If you sense trouble, act promptly. If attacked, push your assaulter aside and shout as loudly as you can. It might not dissuade them, but it will definitely make you seem like a difficult target. Also, it will rouse the attention of people around you and get help to you faster. ";
   _tips.add(tip);
   tip = new Tip();  
 
-tip.title = "Title";
-  tip.imgUrl = "assets/images/tip.jpg";
+tip.title = "Use the heel of your palm to strike at the nose";
+  tip.imgUrl = "assets/images/tip2.jpg";
+  tip.description = "When you're cornered in a deserted area, a swift blow to the nose will help you escape. If your attacker is charging towards you, hit their nose with the heel of your palm using all the strength you've got. This will cause immense pain and will temporarily disorient them, allowing you time to call for help or escape.";
+  _tips.add(tip);
+  tip = new Tip(); 
+
+tip.title = "Use everyday objects as weapons";
+  tip.imgUrl = "assets/images/tip3.jpg";
+  tip.description = "You can fashion weapons out of anything around you. Like any other weapons, a set of keys wedged between your fingers like brass knuckles can deliver a crippling blow. If you are attacked at home, run to the kitchen and grab anything like a rolling pin, heavy ladle or a knife. Just remember to be quick!";
+  _tips.add(tip);
+  tip = new Tip(); 
+
+tip.title = " Get out of a wrist-hold";
+  tip.imgUrl = "assets/images/tip4.jpg";
   tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
   _tips.add(tip);
   tip = new Tip(); 
 
-tip.title = "Title";
-  tip.imgUrl = "assets/images/tip.jpg";
-  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+tip.title = "Keep a bag of essentials in your car";
+  tip.imgUrl = "assets/images/tip5.jpg";
+  tip.description = "A car survival kit can be your best friend when you're driving long distances. Keep items like first aid kit, jumper cables, flashlight, a fully-charged power bank, matchbox, a thick rope etc. in your kit and you'll always be prepared to face any situation on the road.";
   _tips.add(tip);
   tip = new Tip(); 
 
-tip.title = "Title";
-  tip.imgUrl = "assets/images/tip.jpg";
-  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+tip.title = "Hit them with your elbows";
+  tip.imgUrl = "assets/images/tip6.jpg";
+  tip.description = "If the attacker is trying to pin your arms down, this can be a useful strike to incapacitate them with. So yank your arms free of their grapple and hit their head with your elbows as hard as you can. With enough force to the temple, you elbows can potentially cause severe bruising and unconsciousness, crippling your attacker for several minutes.";
   _tips.add(tip);
   tip = new Tip(); 
 
-tip.title = "Title";
-  tip.imgUrl = "assets/images/tip.jpg";
-  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+tip.title = "Use pepper Spray";
+  tip.imgUrl = "assets/images/tip7.jpg";
+  tip.description = "Nothing works more effectively than pepper spray to the face. Some formulations are powerful enough to cause tearing, irritation and even temporary blindness when sprayed directly into the eyes. This trick works best when your attacker is charging towards you. Keep a can of pepper spray in your purse and in the glove compartment of your car for emergencies.";
   _tips.add(tip);
   tip = new Tip(); 
 
-tip.title = "Title";
-  tip.imgUrl = "assets/images/tip.jpg";
-  tip.description = "If your attacker has you in a wrist-hold, here's an easy way to break free. Instead of struggling, squat down, lean forward and then bend your elbows all the way towards their arm, till they can no longer hold on. Or simply rotate your wrist to the point where their finger and thumb meet and yank your wrist free. Their grip will be the weakest at that point and a sharp tug will help you escape quickly.";
+tip.title = "Phone for help when your car breaks down";
+  tip.imgUrl = "assets/images/tip8.jpg";
+  tip.description = "A car breaking down in the middle of nowhere is the textbook example of a difficult situation, but with a little bit of common sense you can easily find a safe way out of the problem. When your car breaks down and you cannot figure it out on your own, simply get back inside, leave your headlights on, lock the doors, stay in & phone for help. Do not step out of the car till proper help has arrived. ";
   _tips.add(tip);
   tip = new Tip(); 
-
+  
   return _tips;
 
 
