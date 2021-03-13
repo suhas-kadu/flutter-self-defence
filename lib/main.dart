7254273a45eb47636173c50a29d6ba3f6fcf052c
suhas-kadu <kadusuhas72@gmail.com>
2021-03-13 14:46:48 +0530

    firease initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:self_defence/views/helpline.dart';
+import 'package:self_defence/views/login_screen.dart';
+import 'package:self_defence/views/registeration_screen.dart';
 import 'package:self_defence/views/self_defence_techniques.dart';
 import 'package:self_defence/views/self_defence_tips.dart';
 import 'package:self_defence/views/technique_demo.dart';
 import 'views/home_page.dart';
 
-void main() => runApp(MyApp());
+
+Future<void> main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
+  runApp(MyApp());
+  }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -18,13 +27,15 @@ class MyApp extends StatelessWidget {
           textTheme: GoogleFonts.montserratTextTheme(
         Theme.of(context).textTheme,
       )),
-      initialRoute: HomePage.id,
+      initialRoute: LoginScreen.id,
       routes: <String, WidgetBuilder>{
         HomePage.id: (context) => HomePage(),
         SelfDefenceTips.id: (context) => SelfDefenceTips(),
         SelfDefenceTechniques.id: (context) => SelfDefenceTechniques(),
         TechniqueDemo.id: (context) => TechniqueDemo(),
         HelpLine.id: (context) => HelpLine(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegisterationScreen.id: (context) => RegisterationScreen()
       },
       // home: HomePage(),
     );
